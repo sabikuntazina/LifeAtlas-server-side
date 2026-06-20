e120be6161fe9025c70282b4bcddc068688a9d41
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-06-21 03:27:35 +0600

    worked on all feature of user's role

--- index.js
+++ index.js
@@ -57,6 +57,9 @@ async function run() {
     const subscriptionsCollection = db.collection("subscriptions");
     const userCollection = db.collection("user");
     const lessonCOllection= db.collection("lessons")
+    const savedLessonCollection=db.collection("savedLessons")
+    const likedLessonCollection=db.collection("likedLessons")
+    const commentCollection=db.collection("comment")
 
     //lessons
         app.post('/lessons',async (req, res) => {  
@@ -70,20 +73,164 @@ async function run() {
 })
 
 app.get("/lessons/all", async (req, res) => {
-      const result = await lessonCOllection.find().toArray();
+      const result = await lessonCOllection.find().sort({ createdAt: -1 }).toArray();
       res.send(result);
     });
+
+app.get("/lessons/:id", async (req, res) => {
+      const {id} = req.params
+      console.log(id)
+      const query= {
+        _id : new ObjectId(id)
+      }
+      const result = await lessonCOllection.findOne(query)
+      res.send(result);
+    });
+
+
+    // saved lessons
+    app.post("/savedlessons",async (req, res) => {  
+    const savedLessonData = req.body;
+   
+    const alreadySaved = await savedLessonCollection.findOne({
+      lessonId: savedLessonData.lessonId,
+      userId: savedLessonData.userId
+    });
+
+   
+    if (alreadySaved) {
+      return res.status(400).send({ 
+        success: false, 
+        message: "You have already saved this lesson!" 
+      });
+    }
+    const newSavedLessonData={
+      ...savedLessonData,
+      createdAt:new Date(),
+    }
+    const result=await savedLessonCollection.insertOne(newSavedLessonData)
+    res.send(result);
+
+    //update lesson for user saved it 
+     const updateLesson = await lessonCOllection.updateOne(
+      {
+        _id: new ObjectId(savedLessonData.lessonId),
+      },
+      {
+      $inc: { saveCount: 1 }
+      }
+    )
+    res.send(updateLesson);
+    
+})
+
+// get favorite lessons from the saved lesson collection
+ app.get("/saved/lessons/favorite/:id", async (req, res) => {
+      
+    const {id} = req.params;
+ const query = { userId: id };
+
+    const result = await savedLessonCollection.find(query).toArray();
+
+    res.send(result);
+  } 
+  );
+
+//Liked Lessons
+    app.post("/likedlessons",async (req, res) => {  
+    const LikedLessonData = req.body;
+
+    const alreadyLiked = await likedLessonCollection.findOne({
+      lessonId: likedLessonData.lessonId,
+      userId: likedLessonData.userId
+    });
+
+    // ২. যদি অলরেডি লাইক দেওয়া থাকে, তবে ৪০০ স্ট্যাটাসসহ ফেরত পাঠানো হবে
+    if (alreadyLiked) {
+      return res.status(400).send({ 
+        success: false, 
+        message: "You have already liked this lesson!" 
+      });
+    }
+    const newLikedLessonData={
+      ...LikedLessonData,
+      createdAt:new Date(),
+    }
+    const result=await likedLessonCollection.insertOne(newLikedLessonData)
+    res.send(result);
+
+    //update lesson for user saved it 
+     const updateLesson = await lessonCOllection.updateOne(
+      {
+        _id: new ObjectId(LikedLessonData.lessonId),
+      },
+      {
+       $inc: { likeCount: 1 }
+      }
+    )
+    res.send(updateLesson);
+})
+//Comment on the Lessons
+    app.post("/comment",async (req, res) => {  
+    const commentData = req.body;
+    const newCommentLessonData={
+      ...commentData,
+      createdAt:new Date(),
+    }
+    const result=await commentCollection.insertOne(newCommentLessonData)
+    res.send(result);
+
+    const newCommentObj = {
+      text: commentData.text,
+      userId: commentData.userId,
+      userName: commentData.userName,
+      userImage: commentData.userImage,
+      userRole: commentData.userRole,
+      userPlan: commentData.userPlan,
+      userEmail: commentData.userEmail,
+      createdAt: new Date(),
+    };
+
+    //update lesson for user saved it 
+     const updateLesson = await lessonCOllection.updateOne(
+      {
+        _id: new ObjectId(commentData.lessonId),
+      },
+      {
+        // $push এর মাধ্যমে 'comments' নামের অ্যারে তৈরি হবে এবং তাতে ডাটা ঢুকবে
+        $push: { comments: newCommentObj }, 
+        
+        // $inc এর মাধ্যমে 'commentCount' ফিল্ডটি না থাকলে তৈরি হবে এবং ১ করে বাড়বে
+        $inc: { commentCount: 1 }          
+      }
+    )
+  
+    if (updateResult.modifiedCount > 0) {
+      res.status(200).send({
+        success: true,
+        message: "Comment added to array successfully!",
+        result: updateResult
+      });
+    } else {
+      res.status(404).send({ success: false, message: "Lesson not found" });
+    }
+    // res.send(updateLesson);
+})
+
+
 
 
 
     //user lessons
 
-    app.get("/lessons/my/:userId", async (req, res) => {
+    app.get("/lessons/my/:creatorId", async (req, res) => {
+       console.log( " request params: ",req.params)
   try {
-    const userId = req.params.userId;
+    const creatorId = req.params.creatorId;
+   
 
     const result = await lessonCOllection
-      .find({ creatorId: userId })
+      .find({ creatorId})
       .sort({ createdAt: -1 })
       .toArray();
 
@@ -246,6 +393,25 @@ app.patch("/lessons/:id", async (req, res) => {
 });
 
 
+app.patch('/profile/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateData = req.body;
+
+    const query = { _id: new ObjectId(id) };
+    
+    const updateDoc = {
+      $set: { ...updateData } 
+    };
+
+    const result = await userCollection.updateOne(query, updateDoc);
+    
+    res.send({ success: true, message: "Profile updated!", result });
+
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
+});
     //Subscription
 
  app.post("/subscription", async (req, res) => {
